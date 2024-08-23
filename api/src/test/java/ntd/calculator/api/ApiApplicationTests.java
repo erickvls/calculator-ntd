a0a8c4af00301b1cd613a786b45fefb124d2bdf3
Erick Sousa <erick.v.l.s@gmail.com>
2024-08-23 07:54:16 -0400

    set up : Create project

---
+++ api/src/test/java/ntd/calculator/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ntd.calculator.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
