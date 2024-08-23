df163a28e46003919b25c587b3a761afed9c9b01
Erick Sousa <erick.v.l.s@gmail.com>
2024-08-23 10:09:49 -0400

    feature: Authentication - Created JWT service for extract username

---
+++ api/src/main/java/ntd/calculator/api/service/JwtService.java
@@ -0,0 +1,10 @@
+package ntd.calculator.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+}
