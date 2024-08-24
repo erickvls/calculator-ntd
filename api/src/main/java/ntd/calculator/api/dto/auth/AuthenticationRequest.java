7ac22a4cc9094b8b148f0cee3524d6e17bb97043
Erick Sousa <erick.v.l.s@gmail.com>
2024-08-24 18:17:13 -0400

    feature: Authentication - Created DTO for request/response auth calls

---
+++ api/src/main/java/ntd/calculator/api/dto/auth/AuthenticationRequest.java
@@ -0,0 +1,13 @@
+package ntd.calculator.api.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+}
