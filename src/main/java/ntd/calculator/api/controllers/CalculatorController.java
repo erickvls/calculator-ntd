6df114bf1c61ffd10f2d625967f32b0afa0dd2f5
Erick Sousa <erick.v.l.s@gmail.com>
2024-09-01 11:13:59 -0400

    feature: Random String controller - Added a new endpoint for generate Strings

--- src/main/java/ntd/calculator/api/controllers/CalculatorController.java
+++ src/main/java/ntd/calculator/api/controllers/CalculatorController.java
@@ -5,14 +5,14 @@ import ntd.calculator.api.models.requests.CalculationRequest;
 import ntd.calculator.api.models.responses.CalculatorResponse;
 import ntd.calculator.api.models.user.User;
 import ntd.calculator.api.services.CalculationService;
+import ntd.calculator.api.services.RandomStringService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/calculator")
@@ -20,12 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculatorController {
 
     private final CalculationService calculationService;
+    private final RandomStringService randomStringService;
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CalculatorResponse> calculate(
             @RequestBody CalculationRequest request,
             @AuthenticationPrincipal User user) {
         var result = calculationService.performOperation(request, user);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result);
+    }
+
+    @GetMapping(value = "/generate/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CalculatorResponse> generateString(@AuthenticationPrincipal User user) throws IOException, InterruptedException {
+        var result = randomStringService.generateRandomString(user);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(result);
