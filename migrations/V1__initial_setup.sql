a1141fab2febc3862aaa6586829ec1b7eace2f8a
Erick Sousa <erick.v.l.s@gmail.com>
2024-09-03 13:52:00 -0400

    feature: Docker - Added docker file and compose for running locally

---
+++ migrations/V1__initial_setup.sql
@@ -0,0 +1,6 @@
+INSERT INTO operation (id, type, cost) VALUES (1, 'ADDITION', 10);
+INSERT INTO operation (id, type, cost) VALUES (2, 'SUBTRACTION', 10);
+INSERT INTO operation (id, type, cost) VALUES (3, 'MULTIPLICATION', 15);
+INSERT INTO operation (id, type, cost) VALUES (4, 'DIVISION', 20);
+INSERT INTO operation (id, type, cost) VALUES (5, 'SQUARE_ROOT', 25);
+INSERT INTO operation (id, type, cost) VALUES (6, 'RANDOM_STRING', 30);
